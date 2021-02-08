2a76cf6c51f265655d579414198c1af0f2dc0ef1
Bryan M <allen.bry95@gmail.com>
2021-02-08 15:47:42 +0100

    feat(App): add Switch and Login route, App linear gradient

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import GlobalStyles from './theme/globalStyles';
+import {Switch, Route} from 'react-router-dom';
+import styled from 'styled-components';
 
+import GlobalStyles from './theme/globalStyles';
+import Login from './components/Login';
+
+const AppStyled = styled.div`
+  background: linear-gradient(to bottom, ${props => props.theme.colors.subLight}, ${props => props.theme.colors.main});
+  min-height: 100vh;
+`;
 
 const App = () => {
   return (
     <>
       <GlobalStyles />
-      <div>
-        My App
-      </div>
+      <AppStyled>
+        <Switch>
+          {/* TODO: redirect to /login if not logged in */}
+          <Route exact path='/' component={Login} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      </AppStyled>
     </>
   );
 }
