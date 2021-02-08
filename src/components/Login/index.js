f6ea27730d808217cb0569941b5d983f4efaea37
Bryan M <allen.bry95@gmail.com>
2021-02-08 17:21:15 +0100

    feat(Login): add Login button, wrapper and title

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import { TitleStyled } from '../shared/Title';
+import Wrapper from '../shared/Wrapper';
+import Button from '../shared/Button';
+
+const LoginTitle = styled(TitleStyled)`
+  text-align: center;
+  font-size: 2.5em;
+
+  @media(min-width: ${(props) => props.theme.media.md}){
+    font-size: 5.625em;
+  }
+`;
+
+const LoginWrapper = styled(Wrapper)`
+  min-height: inherit;
+  justify-content: center;
+  align-items: center;
+  row-gap: 2.5rem;
+`;
 
 const Login = () => (
-  <div>
-    <h1>Login</h1>
-  </div>
+  <LoginWrapper column>
+    <LoginTitle>Spotify React App</LoginTitle>
+    <Button href="#">Se connecter</Button>
+  </LoginWrapper>
 );
 
 export default Login;
