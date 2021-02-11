83f60e064dc59978cba09342c68a6f4acb1ad517
Bryan M <allen.bry95@gmail.com>
2021-02-11 16:24:00 +0100

    feat(Wrapper): add full width and bottom padding on mobile devices

--- src/components/shared/Wrapper.js
+++ src/components/shared/Wrapper.js
@@ -1,8 +1,14 @@
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  display: flex;
+  width: 100%;
+  display: ${(props) => props.flex && 'flex'};
   flex-direction: ${(props) => props.column && 'column'};
+  padding-bottom: ${(props) => props.pb && '10rem'};
+
+  @media (min-width: ${(props) => props.theme.media.lg}) {
+    padding-bottom: 0;
+  }
 `;
 
 export default Wrapper;
