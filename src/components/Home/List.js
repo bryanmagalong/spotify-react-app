5ffbe86677d7be5f8ef593e4948cd1a2cd8933ab
Bryan M <allen.bry95@gmail.com>
2021-02-16 18:27:19 +0100

    feat(List): update grid-template-colums and gap

--- src/components/Home/List.js
+++ src/components/Home/List.js
@@ -3,21 +3,9 @@ import styled from 'styled-components';
 
 const StyledList = styled.div`
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  gap: 10px;
+  grid-template-columns: repeat(auto-fill, minmax(135px, 1fr));
+  gap: 1rem;
   padding: 1rem 0;
-
-  @media (min-width: ${(props) => props.theme.media.md}) {
-    grid-template-columns: repeat(3, 1fr);
-  }
-
-  @media (min-width: ${(props) => props.theme.media.lg}) {
-    grid-template-columns: repeat(4, 1fr);
-  }
-
-  @media (min-width: ${(props) => props.theme.media.xl}) {
-    grid-template-columns: repeat(6, 1fr);
-  }
 `;
 
 const List = ({ children }) => {
