2a484d2ae14566ecb0b6048a793f3f4cffc1ac26
Bryan M <allen.bry95@gmail.com>
2021-02-16 18:30:17 +0100

    feat(playlistMiddleware): add const params

--- src/features/playlists/playlistMiddleware.js
+++ src/features/playlists/playlistMiddleware.js
@@ -6,19 +6,24 @@ export default (store) => (next) => async (action) => {
   switch (action.type) {
     case FETCH_PLAYLISTS:
       try {
-        const urlAPI = 'https://api.spotify.com/v1/me/playlists';
-        const data = await get(urlAPI, '');
+        const params = action.payload ? { params: { ...action.payload } } : {};
+        const data = await get(
+          'https://api.spotify.com/v1/me/playlists',
+          params,
+        );
+
         const playlists = [ ...data.items ].map((item) => {
           return {
             id: item.id,
             href: item.href,
+            type: item.type,
             name: item.name,
             images: item.images[0].url,
             owner: item.owner,
           };
         });
 
-        console.log(playlists);
+        // console.log(data.items);
         store.dispatch(fetchPlaylistSuccess(playlists));
       } catch (error) {
         console.log(error);
