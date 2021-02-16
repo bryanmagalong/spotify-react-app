a5b36b362e6ed5bef52940156999cfe40ce6ccb2
Bryan M <allen.bry95@gmail.com>
2021-02-16 18:29:38 +0100

    feat(playlists): add payload to fetchPlaylists function and add fetch top artists actions

--- src/features/playlists/playlistActions.js
+++ src/features/playlists/playlistActions.js
@@ -1,11 +1,24 @@
 export const FETCH_PLAYLISTS = 'FETCH_PLAYLISTS';
 export const FETCH_PLAYLISTS_SUCCESS = 'FETCH_PLAYLISTS_SUCCESS';
+export const FETCH_TOP_ARTISTS = 'FETCH_TOP_ARTISTS';
+export const FETCH_TOP_ARTISTS_SUCCESS = 'FETCH_TOP_ARTISTS_SUCCESS';
 
-export const fetchPlaylist = () => ({
+export const fetchPlaylist = (payload) => ({
   type: FETCH_PLAYLISTS,
+  payload,
 });
 
 export const fetchPlaylistSuccess = (payload) => ({
+  type: FETCH_PLAYLISTS_SUCCESS,
+  payload,
+});
+
+export const fetchTopArtist = (payload) => ({
+  type: FETCH_PLAYLISTS,
+  payload,
+});
+
+export const fetchTopArtistSuccess = (payload) => ({
   type: FETCH_PLAYLISTS_SUCCESS,
   payload,
 });
