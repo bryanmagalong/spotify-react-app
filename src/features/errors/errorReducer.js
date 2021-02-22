1992bf20cdc220a5d27325215f5a2c0f7a69d4b0
Bryan M <allen.bry95@gmail.com>
2021-02-23 00:43:27 +0100

    feat(errorReducer): update indentation, default case return

--- src/features/errors/errorReducer.js
+++ src/features/errors/errorReducer.js
@@ -1,14 +1,24 @@
 import { HIDE_ERROR, SET_ERROR } from './errorActions';
 
 const initialState = {
-  error: null,
+  error: {},
   isOpen: false,
 };
 
 const errorReducer = (state = initialState, action = {}) => {
-  if (action.type === SET_ERROR) return { error: action.payload, isOpen: true };
-  if (action.type === HIDE_ERROR) return { error: null, isOpen: false };
-  else return initialState;
+  if (action.type === SET_ERROR)
+    return {
+      ...state,
+      error: { ...action.payload },
+      isOpen: true,
+    };
+  if (action.type === HIDE_ERROR)
+    return {
+      ...state,
+      error: {},
+      isOpen: false,
+    };
+  else return state;
 };
 
 export default errorReducer;
