a24593740854803e7159423206272821e7e28020
Bryan M <allen.bry95@gmail.com>
2021-02-22 21:48:36 +0100

    feat(Home): add gap of 2rem on List elements on large sreens and a divider

--- src/components/Home/List.js
+++ src/components/Home/List.js
@@ -3,9 +3,13 @@ import styled from 'styled-components';
 
 const StyledList = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(135px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(140px, 1fr));
   gap: 1rem;
   padding: 1rem 0;
+
+  @media (min-width: ${(props) => props.theme.media.lg}) {
+    gap: 2rem;
+  }
 `;
 
 const List = ({ children }) => {
--- src/components/Home/Section.js
+++ src/components/Home/Section.js
@@ -5,10 +5,18 @@ import { Title } from '../shared/Title';
 
 const StyledSection = styled.section`padding: 1rem 0;`;
 
+const StyledLine = styled.div`
+  width: 100%;
+  height: 1px;
+  background-color: ${(props) => props.theme.colors.gray};
+  margin-top: .8rem;
+`;
+
 const Section = ({ title, children }) => {
   return (
     <StyledSection>
       <Title level="2">{title}</Title>
+      <StyledLine />
       {children}
     </StyledSection>
   );
