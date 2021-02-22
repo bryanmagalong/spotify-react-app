beb4fd422855f66ef55b1f011b1e3933559a2400
Bryan M <allen.bry95@gmail.com>
2021-02-22 23:32:04 +0100

    feat(Home): add top artists and tracks dynamic display

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -10,12 +10,23 @@ import Section from './Section';
 import List from './List';
 import { logout } from '../../features/auth/authActions';
 import { fetchPlaylist } from '../../features/playlists/playlistActions';
+import {
+  fetchMyTopArtists,
+  fetchMyTopTracks,
+} from '../../features/user/userActions';
+
 const Home = () => {
   const dispatch = useDispatch();
   const playlists = useSelector((state) => state.playlists.list);
+  const topTracks = useSelector((state) => state.user.topTracksList);
+  const topArtists = useSelector((state) => state.user.topArtistsList);
 
   useEffect(() => {
-    if (!playlists.length) dispatch(fetchPlaylist({ limit: 4 }));
+    if (!playlists.length) dispatch(fetchPlaylist({ limit: 6 }));
+    if (!topTracks.length)
+      dispatch(fetchMyTopTracks({ limit: 6, time_range: 'long_term' }));
+    if (!topArtists.length)
+      dispatch(fetchMyTopArtists({ limit: 6, time_range: 'long_term' }));
   }, []);
 
   return (
@@ -26,6 +37,16 @@ const Home = () => {
           {playlists.map((playlist) => (
             <Card key={playlist.id} {...playlist} />
           ))}
+        </List>
+      </Section>
+      <Section title="titres les plus écoutés">
+        <List>
+          {topTracks.map((track) => <Card key={track.id} {...track} />)}
+        </List>
+      </Section>
+      <Section title="artistes les plus écoutés">
+        <List>
+          {topArtists.map((artist) => <Card key={artist.id} {...artist} />)}
         </List>
       </Section>
       <Button type="button" onClick={() => dispatch(logout())}>
