e5b1bffd46b28815021751dc0f5ebebac20c2395
Bryan M <allen.bry95@gmail.com>
2021-03-01 14:23:48 +0100

    feat(SearchInput): update uncontrolled to controlled component and add styles

--- src/components/Search/SearchInput.js
+++ src/components/Search/SearchInput.js
@@ -1,13 +1,62 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import styled from 'styled-components';
+import { Search } from 'react-bootstrap-icons';
+import { searchOnChange } from '../../features/search/searchActions';
 
-const StyledSearchInput = styled.div`
+const StyledSearchInput = styled.input`
   background-color: white;
-  width: 35%;
+  width: 100%;
+  height: 100%;
+  border: none;
+  font-size: 1em;
+  font-weight: bold;
+  border-radius: 0 .4rem .4rem 0;
+
+  @media (min-width: ${(props) => props.theme.media.lg}) {
+    width: 60%;
+  }
+`;
+
+const StyledForm = styled.form`
+  height: 100%;
+  display: flex;
+`;
+
+const StyledSearchButton = styled.button`
+  background-color: white;
+  padding: 0 .6rem;
+  font-size: 1em;
+  border-radius: .4rem 0 0 .4rem;
+  border: none;
 `;
 
 const SearchInput = () => {
-  return <StyledSearchInput>Search Input</StyledSearchInput>;
+  const inputValue = useSelector((state) => state.search.value);
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    console.log('Submit event');
+    e.preventDefault();
+  };
+
+  return (
+    <StyledForm role="search" id="searchForm" onSubmit={handleSubmit}>
+      <StyledSearchButton type="submit">
+        <Search />
+      </StyledSearchButton>
+      <StyledSearchInput
+        aria-label="Rechercher un artiste, un titre ou un album"
+        type="text"
+        value={inputValue}
+        placeholder="Artistes, titres ou albums"
+        onChange={(e) => {
+          dispatch(searchOnChange(e.target.value));
+          console.log(e.target.value);
+        }}
+      />
+    </StyledForm>
+  );
 };
 
 export default SearchInput;
