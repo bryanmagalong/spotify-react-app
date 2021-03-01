26c7d9910927eecc5832b0052500a7453ded4a48
Bryan M <allen.bry95@gmail.com>
2021-03-01 16:08:14 +0100

    feat(Card): add Link to playlists/playlistId

--- src/components/shared/Card.js
+++ src/components/shared/Card.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled, { css } from 'styled-components';
 
 const ImageCard = styled.div`
@@ -27,12 +28,12 @@ const DescriptionCard = styled.div`
   ${(props) => props.type === 'artist' && css`text-align: center;`};
 
   & h3 {
-    font-weight: bold;
     margin-bottom: .5rem;
     min-width: 166px;
     overflow: hidden;
     white-space: nowrap;
     text-overflow: ellipsis;
+    font-weight: bold;
   }
 
   & span {
@@ -42,14 +43,25 @@ const DescriptionCard = styled.div`
   }
 `;
 
-const Card = ({ name, images, type, album }) => {
+const StyledCardTitleLink = styled(Link)`
+  color: #FFF;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const Card = ({ name, images, type, album, id }) => {
+  const path = `/${type}s/${id}`;
+
   return (
     <article>
       <ImageCard type={type}>
         <img src={images} alt={name} />
       </ImageCard>
       <DescriptionCard type={type}>
-        <h3>{name}</h3>
+        <h3>
+          <StyledCardTitleLink to={path}>{name}</StyledCardTitleLink>
+        </h3>
         <span>{type === 'track' ? album : type}</span>
       </DescriptionCard>
     </article>
