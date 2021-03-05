edddc5a1697efcb112373aa7102a783d0b2f8420
Bryan M <allen.bry95@gmail.com>
2021-03-05 15:33:42 +0100

    feat(Header): add styles for medium and large sreens

--- src/components/Playlist/Header.js
+++ src/components/Playlist/Header.js
@@ -3,36 +3,75 @@ import styled from 'styled-components';
 
 import { TitleStyled } from '../shared/Title';
 import ImageWrapper from '../shared/ImageWrapper';
+import Wrapper from '../shared/Wrapper';
 
-const StyledHeaderWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const StyledTextWrapper = styled.section`
-  color: #fff;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+const StyledHeaderWrapper = styled(Wrapper)`
   row-gap: 1rem;
 
-  & p {
-    display: none;
+  @media (min-width: ${(props) => props.theme.media.md}) {
+    height: 15rem;
+    flex-direction: row;
+    align-items: flex-end;
+    column-gap: 2rem;
+  }
+`;
+
+const StyledTextWrapper = styled(Wrapper)`
+  color: #fff;
+  height: inherit;
+  row-gap: 1rem;
+
+  @media (min-width: ${(props) => props.theme.media.md}) {
+    align-items: flex-start;
+    justify-content: flex-end;
+  }
+`;
+
+const StyledDescription = styled.p`
+  color: ${(props) => props.theme.colors.gray};
+  display: none;
+
+  @media (min-width: ${(props) => props.theme.media.md}) {
+    display: block;
+  }
+`;
+
+const StyledType = styled.span`
+  display: none;
+  letter-spacing: 3px;
+  text-transform: uppercase;
+
+  @media (min-width: ${(props) => props.theme.media.md}) {
+    display: inline;
   }
 `;
 
 const StyledHeaderTitle = styled(TitleStyled)`
   text-transform: capitalize;
+
+  @media (min-width: ${(props) => props.theme.media.md}) {
+    font-size: 3em;
+  }
+
+  @media (min-width: ${(props) => props.theme.media.xl}) {
+    font-size: ${(props) => props.theme.fontSize.xxl};
+  }
 `;
 
-const Header = ({ name, image, description, owner, followers }) => {
+const Header = ({ name, image, description, owner, followers, type }) => {
   return (
-    <StyledHeaderWrapper>
-      <ImageWrapper src={image} alt={name} />
-      <StyledTextWrapper>
+    <StyledHeaderWrapper flex column itemsCenter>
+      <ImageWrapper
+        marginBottom="0"
+        maxWidth="192px"
+        maxHeight="192px"
+        src={image}
+        alt={name}
+      />
+      <StyledTextWrapper as="section" flex column itemsCenter>
+        <StyledType>{type}</StyledType>
         <StyledHeaderTitle>{name}</StyledHeaderTitle>
-        <p>{description}</p>
+        <StyledDescription>{description}</StyledDescription>
         <span>
           par {owner} • {followers} Abonnés
         </span>
