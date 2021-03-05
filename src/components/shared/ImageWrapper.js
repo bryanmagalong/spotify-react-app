1e24b24008d3b033bb0ebd2b8d33b556bcca511d
Bryan M <allen.bry95@gmail.com>
2021-03-05 15:31:42 +0100

    feat(ImageWrapper): add maxHeight attribute

--- src/components/shared/ImageWrapper.js
+++ src/components/shared/ImageWrapper.js
@@ -3,26 +3,35 @@ import styled from 'styled-components';
 
 const StyledImageWrapper = styled.div.attrs((props) => ({
   maxWidth: props.maxWidth || '50%',
+  maxHeight: props.maxHeight || 'auto',
   marginBottom: props.marginBottom || '1rem',
   imgWidth: props.size ? `${props.size}px` : '100%',
   imgHeight: props.size && `${props.size}px`,
 }))`
   max-width: ${(props) => props.maxWidth};
-  height: auto;
+  max-height: ${(props) => props.maxHeight};
   margin-bottom: ${(props) => props.marginBottom};
 
-  & img {
+  & > img {
     object-fit: cover;
     width: ${(props) => props.imgWidth};
     height: ${(props) => props.size && `${props.size}px`};
   }
 `;
 
-const ImageWrapper = ({ src, alt, marginBottom, maxWidth, size }) => {
+const ImageWrapper = ({
+  src,
+  alt,
+  marginBottom,
+  maxWidth,
+  maxHeight,
+  size,
+}) => {
   return (
     <StyledImageWrapper
       marginBottom={marginBottom}
       maxWidth={maxWidth}
+      maxHeight={maxHeight}
       size={size}
     >
       <img src={src} alt={alt} />
