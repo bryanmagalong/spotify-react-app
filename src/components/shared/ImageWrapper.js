42eaadc8eff5f6e91abc6630b2edc7f3996413ef
Bryan M <allen.bry95@gmail.com>
2021-03-05 22:02:31 +0100

    feat(ImageWrapper): add boxShadow and size attributes and StyledImage, remove imgWidth and imgHeight attributes

--- src/components/shared/ImageWrapper.js
+++ src/components/shared/ImageWrapper.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const StyledImage = styled.img`object-fit: cover;`;
+
 const StyledImageWrapper = styled.div.attrs((props) => ({
   maxWidth: props.maxWidth || '50%',
   maxHeight: props.maxHeight || 'auto',
   marginBottom: props.marginBottom || '1rem',
-  imgWidth: props.size ? `${props.size}px` : '100%',
-  imgHeight: props.size && `${props.size}px`,
+  size: props.size ? `${props.size}px` : '100%',
+  boxShadow: props.boxShadow && '0px 0px 30px 0px rgba(0,0,0,0.50)',
 }))`
   max-width: ${(props) => props.maxWidth};
-  max-height: ${(props) => props.maxHeight};
+  height: ${(props) => props.maxHeight};
   margin-bottom: ${(props) => props.marginBottom};
 
-  & > img {
-    object-fit: cover;
-    width: ${(props) => props.imgWidth};
-    height: ${(props) => props.size && `${props.size}px`};
+  ${StyledImage} {
+    width: ${(props) => props.size};
+    height: ${(props) => props.size};
+    box-shadow: ${(props) => props.boxShadow};
   }
 `;
 
@@ -26,6 +28,7 @@ const ImageWrapper = ({
   maxWidth,
   maxHeight,
   size,
+  boxShadow,
 }) => {
   return (
     <StyledImageWrapper
@@ -33,8 +36,9 @@ const ImageWrapper = ({
       maxWidth={maxWidth}
       maxHeight={maxHeight}
       size={size}
+      boxShadow={boxShadow}
     >
-      <img src={src} alt={alt} />
+      <StyledImage src={src} alt={alt} />
     </StyledImageWrapper>
   );
 };
