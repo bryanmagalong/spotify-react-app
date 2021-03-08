83d4485267d40ca108b8b2c918523b0f28f73f1a
Bryan M <allen.bry95@gmail.com>
2021-03-08 17:28:19 +0100

    feat(TrackItem): add CustomLink iand Wrapper import

--- src/components/Playlist/TrackItem.js
+++ src/components/Playlist/TrackItem.js
@@ -2,43 +2,68 @@ import React from 'react';
 import styled from 'styled-components';
 
 import ImageWrapper from '../shared/ImageWrapper';
+import Wrapper from '../shared/Wrapper';
+import StyledCustomLink from '../shared/CustomLink';
 
-const StyledTrackItem = styled.div`
-  padding: .5rem .8rem;
-  border-radius: 3px;
-  display: grid;
-  grid-template-columns: 1rem 1fr;
-  grid-gap: 1rem;
-  color: ${(props) => props.theme.colors.gray};
+const TrackItem = ({
+  number,
+  images,
+  name,
+  artist,
+  album,
+  explicit,
+  duration,
+}) => {
+  return (
+    <StyledTrackItem>
+      <StyledDiv flex itemsCenter>
+        {number}
+      </StyledDiv>
+      <StyledDiv flex itemsCenter columnGap>
+        <ImageWrapper
+          src={images.url}
+          alt={name}
+          maxWidth="30%"
+          marginBottom="0"
+          size={images.width}
+        />
+        <StyledTitleDiv flex column fullWidth>
+          <StyledText white>{name}</StyledText>
+          <StyledText flex columnGap fontSize=".9rem">
+            <StyledExplicitTag show={explicit}>E</StyledExplicitTag>
+            <StyledCustomLink href={artist.url}>{artist.name}</StyledCustomLink>
+          </StyledText>
+        </StyledTitleDiv>
+      </StyledDiv>
+      <StyledTrackAlbum>{album.name}</StyledTrackAlbum>
+      <StyledTrackDuration>{duration}</StyledTrackDuration>
+    </StyledTrackItem>
+  );
+};
 
-  &:hover {
-    background-color: rgba(178, 178, 178, 0.1);
-  }
-
-  @media (min-width: ${(props) => props.theme.media.md}) {
-    grid-template-columns: ${(props) => props.theme.template.trackGridColumns};
-  }
-`;
-
-const StyledDiv = styled.div`
-  display: flex;
-  align-items: center;
+const StyledDiv = styled(Wrapper)`
   column-gap: ${(props) => props.columnGap && '1rem'};
 `;
 
-const StyledTitleDiv = styled(StyledDiv)`
-  width: 100%;
-  flex-direction: column;
+const StyledTitleDiv = styled(Wrapper)`
   align-items: flex-start;
   row-gap: .3rem;
 `;
 
 const StyledText = styled.div`
   letter-spacing: .1rem;
-  color: ${(props) => props.white && '#FFF'};
+  color: ${(props) => (props.white ? '#FFF' : props.theme.colors.gray)};
   display: ${(props) => props.flex && 'flex'};
   column-gap: ${(props) => props.columnGap && '.5rem'};
   font-size: ${(props) => (props.fontSize ? props.fontSize : '1rem')};
+
+  ${StyledCustomLink} {
+    color: ${(props) => props.theme.colors.gray};
+
+    &:hover {
+      color: #fff;
+    }
+  }
 `;
 
 const StyledExplicitTag = styled.span`
@@ -65,38 +90,21 @@ const StyledTrackDuration = styled(StyledTrackAlbum)`
   }
 `;
 
-const TrackItem = ({
-  number,
-  images,
-  name,
-  artist,
-  album,
-  explicit,
-  duration,
-}) => {
-  return (
-    <StyledTrackItem>
-      <StyledDiv>{number}</StyledDiv>
-      <StyledDiv columnGap>
-        <ImageWrapper
-          src={images.url}
-          alt={name}
-          maxWidth="30%"
-          marginBottom="0"
-          size={images.width}
-        />
-        <StyledTitleDiv>
-          <StyledText white>{name}</StyledText>
-          <StyledText flex columnGap fontSize=".9rem">
-            <StyledExplicitTag show={explicit}>E</StyledExplicitTag>
-            {artist.name}
-          </StyledText>
-        </StyledTitleDiv>
-      </StyledDiv>
-      <StyledTrackAlbum>{album.name}</StyledTrackAlbum>
-      <StyledTrackDuration>{duration}</StyledTrackDuration>
-    </StyledTrackItem>
-  );
-};
+const StyledTrackItem = styled.div`
+  padding: .5rem .8rem;
+  border-radius: 3px;
+  display: grid;
+  grid-template-columns: 1rem 1fr;
+  grid-gap: 1rem;
+  color: ${(props) => props.theme.colors.gray};
+
+  &:hover {
+    background-color: rgba(178, 178, 178, 0.1);
+  }
+
+  @media (min-width: ${(props) => props.theme.media.md}) {
+    grid-template-columns: ${(props) => props.theme.template.trackGridColumns};
+  }
+`;
 
 export default TrackItem;
