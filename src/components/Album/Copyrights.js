d139bb445349b8002537ea4a011ff71e608f2271
Bryan M <allen.bry95@gmail.com>
2021-03-10 16:42:03 +0100

    feat: add Coyprights component

---
+++ src/components/Album/Copyrights.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Wrapper from '../shared/Wrapper';
+
+const Copyrights = ({ copyrights }) => {
+  return (
+    <StyledCopyrights as="section" flex column>
+      {copyrights.map((item) => <p key={item}>{item}</p>)}
+    </StyledCopyrights>
+  );
+};
+
+const StyledCopyrights = styled(Wrapper)`
+  color: ${(props) => props.theme.colors.gray};
+  row-gap: .5rem;
+  padding-bottom: 1.5rem;
+  font-size: ${(props) => props.theme.fontSize.md}
+`;
+
+export default Copyrights;
--- src/components/Album/index.js
+++ src/components/Album/index.js
@@ -7,12 +7,15 @@ import { fetchAlbumById } from '../../features/albums/albumActions';
 import Header from '../shared/Header';
 import TrackList from '../shared/TrackList';
 import TrackItem from '../shared/TrackItem';
+import Copyrights from './Copyrights';
 
 const Album = () => {
   const { albumId } = useParams();
+  console.log(albumId);
   const dispatch = useDispatch();
   const album = useSelector((state) => state.albums.album);
   const tracks = useSelector((state) => state.albums.trackList);
+  const copyrights = useSelector((state) => state.albums.copyrights);
 
   useEffect(
     () => {
@@ -20,6 +23,7 @@ const Album = () => {
     },
     [ albumId, dispatch ],
   );
+
   return (
     <Wrapper px pb pt>
       <Header {...album} image={album.images} />
@@ -28,6 +32,7 @@ const Album = () => {
           <TrackItem key={item.id} number={index + 1} {...item} />
         ))}
       </TrackList>
+      <Copyrights copyrights={copyrights} />
     </Wrapper>
   );
 };
