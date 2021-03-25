b355e9f1610db1d51789734859d7057a5d33cd3a
Bryan M <allen.bry95@gmail.com>
2021-03-25 15:53:35 +0100

    feat(browseMiddleware): add country and locale parameters on FETCH_ALL_CATEGORIES api url

--- src/features/browse/browseMiddleware.js
+++ src/features/browse/browseMiddleware.js
@@ -8,9 +8,12 @@ const browseMiddleware = (store) => (next) => async (action) => {
   switch (action.type) {
     case FETCH_ALL_CATEGORIES:
       try {
-        const data = await get('https://api.spotify.com/v1/browse/categories', {
-          params: { limit: 40 },
-        });
+        const data = await get(
+          'https://api.spotify.com/v1/browse/categories?country=FR&locale=fr_FR',
+          {
+            params: { limit: 40 },
+          },
+        );
         const categories = [ ...data.categories.items ];
 
         console.log(data);
