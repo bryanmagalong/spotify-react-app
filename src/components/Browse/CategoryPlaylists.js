0e1e3fa592e9f9fe5998da958b549354b8baa5b0
Bryan M <allen.bry95@gmail.com>
2021-04-13 19:05:03 +0200

    feat(CategoryPlaylists): add dynamic header color display

--- src/components/Browse/CategoryPlaylists.js
+++ src/components/Browse/CategoryPlaylists.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
+import { usePalette } from 'react-palette';
 
 import Wrapper from '../shared/Wrapper';
 import { StyledTitle } from '../shared/Title';
@@ -12,8 +13,10 @@ import { fetchCategoryPlaylistsById } from '../../features/browse/browseActions'
 const CategoryPlaylists = () => {
   const { categoryId } = useParams();
   const categoryName = useSelector((state) => state.browse.categoryName);
+  const imageUrl = useSelector((state) => state.browse.imageUrl);
   const playlists = useSelector((state) => state.browse.playlists);
   const dispatch = useDispatch();
+  const { data } = usePalette(imageUrl);
 
   useEffect(
     () => {
@@ -23,22 +26,50 @@ const CategoryPlaylists = () => {
   );
 
   return (
-    <CategoryHeader px pt pb>
-      <section>
+    <Wrapper>
+      <CategoryHeader as="section" flex color={data.vibrant} pt>
         <Title>{categoryName}</Title>
-      </section>
-      <section>
+      </CategoryHeader>
+      <Wrapper as="section" px pb>
         <List>
           {playlists.map((playlist) => (
             <Card key={playlist.id} {...playlist} />
           ))}
         </List>
-      </section>
-    </CategoryHeader>
+      </Wrapper>
+    </Wrapper>
   );
 };
-const Title = styled(StyledTitle)``;
+const Title = styled(StyledTitle)`
+  padding-bottom: 1rem;
+  padding-left: 1rem;
+  padding-right: 1rem;
 
-const CategoryHeader = styled(Wrapper)``;
+  @media (min-width: ${(props) => props.theme.media.md}) {
+    font-size: 2.3em;
+  }
+
+  @media (min-width: ${(props) => props.theme.media.lg}) {
+    padding-left: 2rem;
+    padding-right: 2rem;
+  }
+
+  @media (min-width: ${(props) => props.theme.media.xl}) {
+    font-size: ${(props) => props.theme.fontSize.xxl};
+  }
+`;
+
+const CategoryHeader = styled(Wrapper)`
+  background: linear-gradient(0deg, rgba(21, 21, 21, 1), transparent)
+    ${(props) => (props.color ? props.color : props.theme.colors.green)};
+  height: 8rem;
+  align-items: flex-end;
+
+  @media (min-width: ${(props) => props.theme.media.md}) {
+    height: 15rem;
+    align-items: flex-end;
+    column-gap: 2rem;
+  }
+`;
 
 export default CategoryPlaylists;
