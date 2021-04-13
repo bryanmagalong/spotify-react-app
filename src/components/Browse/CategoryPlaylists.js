3698074fa36573cde4953c27c4ad66abafcd833a
Bryan M <allen.bry95@gmail.com>
2021-04-13 18:21:45 +0200

    feat(CategoryPlaylists): add category playlists display

--- src/components/Browse/CategoryPlaylists.js
+++ src/components/Browse/CategoryPlaylists.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+
+import Wrapper from '../shared/Wrapper';
+import { StyledTitle } from '../shared/Title';
+import List from '../shared/List';
+import Card from '../shared/Card';
+import { fetchCategoryPlaylistsById } from '../../features/browse/browseActions';
 
 const CategoryPlaylists = () => {
-  return <div>Category Playlists</div>;
+  const { categoryId } = useParams();
+  const categoryName = useSelector((state) => state.browse.categoryName);
+  const playlists = useSelector((state) => state.browse.playlists);
+  const dispatch = useDispatch();
+
+  useEffect(
+    () => {
+      dispatch(fetchCategoryPlaylistsById(categoryId));
+    },
+    [ dispatch, categoryId, playlists.length ],
+  );
+
+  return (
+    <CategoryHeader px pt pb>
+      <section>
+        <Title>{categoryName}</Title>
+      </section>
+      <section>
+        <List>
+          {playlists.map((playlist) => (
+            <Card key={playlist.id} {...playlist} />
+          ))}
+        </List>
+      </section>
+    </CategoryHeader>
+  );
 };
+const Title = styled(StyledTitle)``;
+
+const CategoryHeader = styled(Wrapper)``;
 
 export default CategoryPlaylists;
