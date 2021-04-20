4a42576f24327f1ed6d2c2a519d2347108eeb9bf
Bryan M <allen.bry95@gmail.com>
2021-04-20 22:50:10 +0200

    feat(CategoryCard): add category card dynamic color

--- src/components/Browse/CategoryCard.js
+++ src/components/Browse/CategoryCard.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { usePalette } from 'react-palette';
 
 import { StyledTitle as Title } from '../shared/Title';
+import { fetchCategoryColor } from '../../features/browse/browseActions';
 
 const CategoryCard = ({ name, id, icons }) => {
   const categoryLink = `/browse/categories/${id}/playlists`;
+  const dispatch = useDispatch();
+  const categoryImageUrl = useSelector(
+    (state) =>
+      state.browse.categories.find((category) => id === category.id).imageUrl,
+  );
+  const { data } = usePalette(categoryImageUrl);
+  useEffect(
+    () => {
+      if (!categoryImageUrl) dispatch(fetchCategoryColor(id));
+    },
+    [ dispatch, id, categoryImageUrl ],
+  );
 
   return (
-    <StyledCard as={Link} to={categoryLink} icon={icons[0].url}>
+    <StyledCard
+      as={Link}
+      to={categoryLink}
+      icon={icons[0].url}
+      color={data.vibrant}
+    >
       <StyledTitle as="h3">{name}</StyledTitle>
     </StyledCard>
   );
@@ -23,7 +43,7 @@ const StyledTitle = styled(Title)`
 const StyledCard = styled.div`
   height: 5rem;
   background: linear-gradient(160deg, rgba(0, 0, 0, 0.5), transparent)
-    ${(props) => props.theme.colors.green};
+    ${(props) => props.color};
   border-radius: .5rem;
 
   &:hover {
