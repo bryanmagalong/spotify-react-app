331719d94b96c916ddc10201d809f5d312a43f9d
Bryan M <allen.bry95@gmail.com>
2021-05-07 22:10:28 +0200

    feat(List): add gutter props

--- src/components/shared/List.js
+++ src/components/shared/List.js
@@ -4,7 +4,7 @@ const List = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(128px, 1fr));
   gap: 1rem;
-  padding: 1rem 0;
+  padding: ${props => props.gutter ?? "1rem 0"};
 
   @media (min-width: ${(props) => props.theme.media.md}) {
     grid-template-columns: repeat(auto-fill, minmax(140px, 1fr));
